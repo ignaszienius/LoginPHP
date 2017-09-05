adde1ce213f44bdb7cbdb228a3b8978892f60f5e
ignaszienius <ignaszienius@gmail.com>
2017-09-05 21:09:49 +0300

    Veikia login jau

--- View/main.php
+++ View/main.php
@@ -1,40 +1,19 @@
-<?php
-
-$vardas = "";
-
-#COOKIES
-
-/*setcookie("username", "Ignas", time() + (86400 * 7), "/"); // 86400 = 1 day
-if(isset($_COOKIE["username"])) {
-	$vardas = $_COOKIE["username"];
-} else {
-	$vardas = "nežinomas";
-}
-*/
-#SESSIONS
-session_start();
-//$_SESSION['username'] = "Antanas";
-if(isset($_SESSION["username"])) {
-	$vardas = $_SESSION["username"];
-} else {
-	$vardas = "nežinomas";
-}
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Cookies and sessions</title>
+	<title>Main page</title>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
 <div class="container-fluid">
 	<div class="row head bg-warning">
-		<h1>Cookies and sessions</h1>
+		<h1>Main page</h1>
 	</div>
 	<div class="col">
-		Mano vardas yra <?= $vardas; ?>.
+		<?php 
+			echo "Sveiki, " .  $_SESSION['user']['username'];
+		 ?>.
 	</div>
 </div>
 </body>
--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-include "view/login.php";
+session_start();
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+	if($_POST['username'] == "Ignas" && $_POST['password'] == "slaptas") {
+		#tinka username is password
+		$_SESSION['user']['username'] = $_POST['username'];
+		$_SESSION['user']['level'] = "admin";
+	} else {
+		#netinka username is password
+		$error = "Wrong username or password";
+	}
+} else {
+	#prisijungimo duomenys nebuvo ivesti
+	$error = "No login data!";
+}
+
+// Login check (ar vartotojas prisijunges)
+
+if (isset($_SESSION['user'])) {
+	include "view/main.php";
+} else {
+	include "view/login.php";
+}
+
+
+
+
+
+
